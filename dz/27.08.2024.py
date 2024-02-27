797ae8481ab1235a97e17cd6069b5ea896b14e68
Gehaso90 <urakucenko13@gmail.com>
2024-02-27 19:15:46 +0300

    teee

--- dz/27.08.2024.py
+++ dz/27.08.2024.py
@@ -29,6 +29,40 @@
 
 
 
+# def get_common_symbols(files):
+#     common_symbols = set(files[0])
+#     for file in files[1:]:
+#         common_symbols &= set(file)
+#     return common_symbols
+
+# def read_files(file_names):
+#     files = []
+#     for file_name in file_names:
+#         with open(file_name, 'r') as file:
+#             files.append(file.read())
+#     return files
+
+# def write_common_symbols(common_symbols, output_file):
+#     with open(output_file, 'w') as file:
+#         for symbol in common_symbols:
+#             file.write(symbol)
+
+# def main():
+#     file_names = []
+#     while True:
+#         file_name = input("Enter file name (or 'quit' to stop): ")
+#         if file_name.lower() == 'quit':
+#             break
+#         file_names.append(file_name)
+
+#     files = read_files(file_names)
+#     common_symbols = get_common_symbols(files)
+#     output_file = input("Enter output file name: ")
+#     write_common_symbols(common_symbols, output_file)
+
+# if __name__ == "__main__":
+#     main()
+
 
 
 
